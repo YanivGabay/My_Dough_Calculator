a01759497ff0f1931cfa77708b9d7aa8b25f45ca
yaniv <yaniv242@gmail.com>
2025-06-13 00:59:51 +0300

    git hub pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my_dough_calculator/', // Replace with your GitHub repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
